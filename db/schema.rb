4ea2bd145e7e242f87f5dfa9d383eb6e3aa09f6f
tomohalloran@outlook.com <tomohalloran@outlook.com>
2018-12-13 15:56:56 +0000

    install bootstrap.

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,6 @@
 ActiveRecord::Schema.define(version: 2018_12_11_180728) do
 
   create_table "articles", force: :cascade do |t|
-    t.string "title"
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
